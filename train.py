0224f71f165e5fdcc7cfe4b23079b809ea140c5b
MOKOBAKUMAN <66781207+MOKOBAKUMAN@users.noreply.github.com>
2022-08-19 00:41:13 +0900

    Update train.py (epoch 100 --> 500)
    (epoch 100 --> 500)

--- train.py
+++ train.py
@@ -84,7 +84,7 @@ def _main():
             steps_per_epoch=max(1, num_train//batch_size),
             validation_data=data_generator_wrapper(lines[num_train:], batch_size, input_shape, anchors, num_classes),
             validation_steps=max(1, num_val//batch_size),
-            epochs=100,
+            epochs=500,
             initial_epoch=50,
             callbacks=[logging, checkpoint, reduce_lr, early_stopping])
         model.save_weights(log_dir + 'trained_weights_final.h5')
